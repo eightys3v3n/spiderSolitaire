a6cb73cc07fd0bd73b2bd9a417fc049fa1b48449
Terrence Plunkett <eightys3v3n+github@gmail.com>
2016-05-23 13:53:45 -0600

    moved variable declarations to main.hpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-#ifdef TESTS
-#include "tests.hpp"
-#endif // TESTS
-
 #include <iostream>
 #include <vector>
 #include <map>
 #include <chrono>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include "main.hpp"
 #include "draw.hpp"
 #include "input.hpp"
 #include "cardStructure.hpp"
@@ -16,29 +13,6 @@
 using namespace std;
 using namespace sf;
 using namespace std::chrono;
-
-RenderWindow window;
-RectangleShape newGameButton, topBar;
-Image textureFile;
-vector< RectangleShape > layers(5);
-vector< RectangleShape > completedStacks(8);
-vector< Card > cards;
-vector< Card* > unusedCards;
-vector< Texture > textures;
-vector< vector< Card* > > board;
-vector< FloatRect > columbs;
-Vector2i clickedCard, moveTo;
-Vector2f clickOffset;
-high_resolution_clock::time_point timeClicked;
-int suit = 1; // 0-clubs, 1-spades
-bool playing = false;
-bool running = true;
-bool holding = false;
-unsigned int layersToDraw = 5;
-unsigned int completedStacksToDraw = 0;
-Vector2i texturesStart( 1, 1 );
-Vector2i cardSize( 72, 96 );
-Vector2i cardSpacing( 1, 1 );
 
 void initialize()
 {
