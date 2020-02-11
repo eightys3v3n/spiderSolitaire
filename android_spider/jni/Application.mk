fc536eea41aba399521bebebaea6e905be0c281c
EightyS3v3n <eightys3v3n@gmail.com>
2020-02-11 11:28:25 -0700

    No idea

--- android_spider/jni/Application.mk
+++ android_spider/jni/Application.mk
@@ -1,8 +1,8 @@
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-26
-#APP_STL := c++_shared
-APP_STL := gnustl_static
+APP_STL := c++_static
+#APP_STL := gnustl_static
 #APP_STL := stlport_static
 APP_CPPFLAGS := -std=c++11
-APP_ABI := armeabi
+APP_ABI := all
 APP_MODULES := sfml-activity sfml-example
